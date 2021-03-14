2bbfcf00f4312f3b52ed799a42cf3d4ee3fb413b
gz65536 <gz65536@users.noreply.github.com>
2021-03-14 11:42:54 +0800

    Update YieldToMaturity.py

--- YieldToMaturity.py
+++ YieldToMaturity.py
@@ -1,16 +1,17 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
 # Draws two functions that show the problem in the same plot
-
-# 100 linearly spaced numbers
-x = np.linspace(0,20,100) # should be adjusted flexibly according to different data
 
 # some constants to use
 FV = 1000 # face value
 C = 50 # coupon payment
 P = 900 # current price
 n = 10 # number of payments
+
+# 100 linearly spaced numbers
+x = np.linspace(0,math.ceil(FV/C),num=500) # should be adjusted flexibly according to different data
 
 # We are looking for Y, i.e. yield to maturity
 # Which can be expressed as two functions between y and x
@@ -24,11 +25,20 @@ z = (FV-P) / (C*x - FV) + 1
 
 fig = plt.figure()
 # ax = fig.add_subplot(1, 1, 1)
+# ax.spines['bottom'].set_position('zero')
 
 # plot functions
 plt.plot(x,y, 'r')
 plt.plot(x,z, 'g')
+plt.ylim([0, 1])
+
+# plot intersection
+# Find the index at which `y-z` is nearest to 0
+ind_cross = np.argmin(np.fabs(y-z))
+# Plot vertical line there
+plt.axvline(x[ind_cross], color='0.5', ls=':', label='Y='+'{:.2f}'.format(100/x[ind_cross])+'%')
 
 # show the plot
 plt.grid()
+plt.legend()
 plt.show()
